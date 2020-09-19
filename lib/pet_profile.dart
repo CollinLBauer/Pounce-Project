e145aad891b234db8ec0e0c6b011f87743d4e99a
Isabel Lally <lallyir@g.cofc.edu>
2020-09-19 19:33:23 -0400

    Added scrolling function

--- lib/pet_profile.dart
+++ lib/pet_profile.dart
@@ -17,32 +17,33 @@ class _Pet_ProfileState extends State<Pet_Profile> {
 //EchoAR echo = EchoAR(apiKey: "<Key>");
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Container(
-                color: Colors.tealAccent[100],
-                padding: EdgeInsets.all(2),
-                child: IconButton(
-                  icon: Icon(Icons.favorite),
-                  iconSize: 40,
-                  color: Colors.red,
-                  onPressed: (){
-                    // to hearted page
-                    Navigator.pop(context);
-                  },
-                ),
-              ),
-              Expanded(
-                child:Container(
-                  padding: EdgeInsets.all(30),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Container(
                   color: Colors.tealAccent[100],
+                  padding: EdgeInsets.all(2),
+                  child: IconButton(
+                    icon: Icon(Icons.favorite),
+                    iconSize: 40,
+                    color: Colors.red,
+                    onPressed: (){
+                      // to hearted page
+                      Navigator.pop(context);
+                    },
+                  ),
                 ),
-              ),
-            ],
-          ),
-          Container(
+                Expanded(
+                  child:Container(
+                    padding: EdgeInsets.all(30),
+                    color: Colors.tealAccent[100],
+                  ),
+                ),
+              ],
+            ),
+            Container(
               child: Row(
                 children: <Widget>[
                   Text(
@@ -53,80 +54,81 @@ class _Pet_ProfileState extends State<Pet_Profile> {
                     ", 1", //AGE
                     style: TextStyle(fontSize: 70),
                   ),
-            ],
-          ),
-        ),
-          Container(
-            color: Colors.blueGrey[100],
-            padding: EdgeInsets.all(10),
-            child: Image(
-              image: AssetImage('assets/pets/Dory.jpg'), //IMAGE
-              height: 375,
-              width:375,
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(0),
-            child: Row(
-                children: <Widget>[
-                  Text(
-                    "Location: Charleston Animal Society", //Shelter
-                    style: TextStyle(fontSize: 15),
-                  ),
-                ]
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  "Likes: Treats, Cuddles, Toys on Strings", //LIKES
-                  style: TextStyle(fontSize: 20),
-                ),
-              ]
-            )
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: <Widget>[
-                Text(
-                  "Dislikes: Vacuum Cleaners", //DISLIKES
-                  style: TextStyle(fontSize: 20),
-                ),
-              ]
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: <Widget>[
-                Text(
-                "Deal Breakers: Children", //DEAL BREAKERS
-                style: TextStyle(color: Colors.red, fontSize: 20),
-                ),
-            ]
-          ),
-          ),
-          Container(
-            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-            child: ButtonTheme(
-              height: 55,
-              minWidth: 200,
-              child: FlatButton(
-                  child: Text(
-                    "ADOPT ME!",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),
-                  ),
-                  color: Colors.tealAccent,
-                  onPressed: (){
-                    launch(Uri.encodeFull(url));
-                  },
+                ],
               ),
-            )
-          ),
-        ],
+            ),
+            Container(
+              color: Colors.blueGrey[100],
+              padding: EdgeInsets.all(10),
+              child: Image(
+                image: AssetImage('assets/pets/Dory.jpg'), //IMAGE
+                height: 375,
+                width:375,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(0),
+              child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Location: Charleston Animal Society", //Shelter
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ]
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.all(10),
+                child: Row(
+                    children: <Widget>[
+                      Text(
+                        "Likes: Treats, Cuddles, Toys on Strings", //LIKES
+                        style: TextStyle(fontSize: 20),
+                      ),
+                    ]
+                )
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Dislikes: Vacuum Cleaners", //DISLIKES
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ]
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Row(
+                  children: <Widget>[
+                    Text(
+                      "Deal Breakers: Children", //DEAL BREAKERS
+                      style: TextStyle(color: Colors.red, fontSize: 20),
+                    ),
+                  ]
+              ),
+            ),
+            Container(
+                padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+                child: ButtonTheme(
+                  height: 55,
+                  minWidth: 200,
+                  child: FlatButton(
+                    child: Text(
+                      "ADOPT ME!",
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.deepPurpleAccent),
+                    ),
+                    color: Colors.tealAccent,
+                    onPressed: (){
+                      launch(Uri.encodeFull(url));
+                    },
+                  ),
+                )
+            ),
+          ],
+        ),
       ),
     );
   }
